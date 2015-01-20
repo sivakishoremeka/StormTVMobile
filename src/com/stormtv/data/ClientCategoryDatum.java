062b602e6e5bcdb1210d7c05c111f1783c4f598d
sivakishore <kishoremekas@gmail.com>
2015-01-20 22:09:27 +0530

    Initial Commit
    Initial Commit

---
+++ src/com/stormtv/data/ClientCategoryDatum.java
@@ -0,0 +1,24 @@
+package com.stormtv.data;
+
+public class ClientCategoryDatum {
+
+	private long id;
+	private String categoryType;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCategoryType() {
+		return categoryType;
+	}
+
+	public void setCategoryType(String categoryType) {
+		this.categoryType = categoryType;
+	}
+
+}
---
+++ src/com/stormtv/data/SenderMailId.java
@@ -0,0 +1,13 @@
+package com.stormtv.data;
+
+import com.google.gson.annotations.SerializedName;
+
+public class SenderMailId {
+	
+	@SerializedName("uniqueReference")
+	public String mailId;
+
+	public SenderMailId(String mailId){
+		this.mailId = mailId;
+	}
+}
